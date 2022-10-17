9a25194546731c12dd8e8f41d2efe0c9a99fd608
Hiruja-Bot-max <115850671+Hiruja-Bot-max@users.noreply.github.com>
2022-10-17 22:46:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,19 +54,19 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94714188730'] //ur owner number
-global.ownername = "nipuna rangana" //ur owner name
-global.ytname = "YT: 𝐂𝐈𝐁𝐄𝐑 𝐓𝐄𝐂𝐇" //ur yt chanel name
-global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
+global.vcardowner = ['94753562916'] //ur owner number
+global.ownername = "kavindu " //ur owner name
+global.group = "wp: kavindu " //ur yt chanel name
+global.socialm = "GitHub: kavi-apps" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy
-global.owner = ['94714188730'] //ur number 
-global.ownertag = '94714188730' //ur tag number
+global.owner = ['94753562916'] //ur number 
+global.ownertag = '94753562916' //ur tag number
 global.botname = '📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶' //ur bot name
-global.linkz = "https://youtu.be/3J9-8U8St3U" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/3J9-8U8St3U" //ur website to be displayed
-global.botscript = 'https://github.com/Nipuna-apps/Blue-Lione-Bot' //script link
+global.linkz = "https://chat.whatsapp.com/Jk8H3tTbUbKGZtPwAjDuR7" //your theme url which will be displayed on whatsapp
+global.websitex = "https://chat.whatsapp.com/Jk8H3tTbUbKGZtPwAjDuR7" //ur website to be displayed
+global.botscript = 'https://github.com/Hiruja-Bot-max/Kavi-BLUE-LION-BOT' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶" //ur sticker watermark packname
